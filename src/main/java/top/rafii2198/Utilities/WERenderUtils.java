528ad72acf78815768a32430ecac91767a4a82e1
Rafii2198 <rafii2198@gmail.com>
2026-01-05 15:31:28 +0100

    initial 1.21.11 port

--- src/main/java/top/rafii2198/Utilities/WERenderUtils.java
+++ src/main/java/top/rafii2198/Utilities/WERenderUtils.java
@@ -1,20 +1,16 @@
 package top.rafii2198.Utilities;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.wynntils.utils.MathUtils;
 import com.wynntils.utils.colors.CustomColor;
-import com.wynntils.utils.render.buffered.CustomRenderType;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.resources.ResourceLocation;
-import org.joml.Matrix4f;
+import com.wynntils.utils.render.RenderUtils;
+import com.wynntils.utils.render.pipelines.CustomRenderPipelines;
+import net.minecraft.client.gui.GuiGraphics;
 import top.rafii2198.wynntils.core.type.FlatBarTexture;
 
 public class WERenderUtils {
 
     public static void drawColoredFlatProgressBar(
-            PoseStack poseStack,
-            MultiBufferSource bufferSource,
+            GuiGraphics guiGraphics,
             FlatBarTexture texture,
             CustomColor color,
             float x1,
@@ -23,88 +19,41 @@ public class WERenderUtils {
             float y2,
             float progress) {
 
-        drawColoredBarBackground(
-                poseStack,
-                bufferSource,
-                texture.getResource(),
+        RenderUtils.drawTexturedRect(
+                guiGraphics,
+                CustomRenderPipelines.PROGRESS_BAR_PIPELINE,
+                texture.getIdentifier(),
                 color,
                 x1,
                 y1,
                 x2,
                 y2,
-                texture.X1(),
+                0,
                 texture.backgroundY1(),
-                texture.X2(),
-                texture.backgroundY2());
+                texture.textureWidth(),
+                texture.backgroundY2() - texture.backgroundY1(),
+                texture.textureWidth(),
+                texture.textureHeight());
 
-        float progressLeft = progress;
-        while (progressLeft > 0) {
-            drawColoredBarForeground(
-                    poseStack,
-                    bufferSource,
-                    texture.getResource(),
+        while (progress > 0) {
+            RenderUtils.drawTexturedRect(
+                    guiGraphics,
+                    CustomRenderPipelines.PROGRESS_BAR_PIPELINE,
+                    texture.getIdentifier(),
                     color,
                     x1,
                     y1,
-                    x2,
+                    x2 * MathUtils.clamp(progress, 0, 1),
                     y2,
-                    texture.X1(),
+                    0,
                     texture.foregroundY1(),
-                    texture.X2(),
-                    texture.foregroundY2(),
-                    progressLeft);
-            progressLeft -= 1;
-            if (progressLeft < 0) break;
+                    texture.textureWidth() * MathUtils.clamp(progress, 0, 1),
+                    texture.foregroundY2() - texture.foregroundY1(),
+                    texture.textureWidth(),
+                    texture.textureHeight());
+
+            progress -= 1;
             color = color.hueShift(0.085f);
         }
-    }
-
-    private static void drawColoredBarBackground(
-            PoseStack poseStack,
-            MultiBufferSource bufferSource,
-            ResourceLocation resourceLocation,
-            CustomColor color,
-            float x1,
-            float y1,
-            float x2,
-            float y2,
-            float textureX1,
-            float textureY1,
-            float textureX2,
-            float textureY2) {
-        Matrix4f matrix = poseStack.last().pose();
-        VertexConsumer buffer = bufferSource.getBuffer(CustomRenderType.getPositionColorTextureQuad(resourceLocation));
-
-        buffer.addVertex(matrix, x1, y1, 0).setUv(textureX1, textureY1).setColor(color.asInt());
-        buffer.addVertex(matrix, x1, y2, 0).setUv(textureX1, textureY2).setColor(color.asInt());
-        buffer.addVertex(matrix, x2, y2, 0).setUv(textureX2, textureY2).setColor(color.asInt());
-        buffer.addVertex(matrix, x2, y1, 0).setUv(textureX2, textureY1).setColor(color.asInt());
-    }
-
-    private static void drawColoredBarForeground(
-            PoseStack poseStack,
-            MultiBufferSource bufferSource,
-            ResourceLocation resourceLocation,
-            CustomColor color,
-            float x1,
-            float y1,
-            float x2,
-            float y2,
-            float textureX1,
-            float textureY1,
-            float textureX2,
-            float textureY2,
-            float progress) {
-        Matrix4f matrix = poseStack.last().pose();
-        VertexConsumer buffer = bufferSource.getBuffer(CustomRenderType.getPositionColorTextureQuad(resourceLocation));
-
-        float adjustedProgress = MathUtils.clamp(progress, 0, 1);
-        float tx2 = textureX2 - (1f - adjustedProgress) * (textureX2 - textureX1);
-        x2 = x2 - (1f - adjustedProgress) * (x2 - x1);
-
-        buffer.addVertex(matrix, x1, y1, 0).setUv(textureX1, textureY1).setColor(color.asInt());
-        buffer.addVertex(matrix, x1, y2, 0).setUv(textureX1, textureY2).setColor(color.asInt());
-        buffer.addVertex(matrix, x2, y2, 0).setUv(tx2, textureY2).setColor(color.asInt());
-        buffer.addVertex(matrix, x2, y1, 0).setUv(tx2, textureY1).setColor(color.asInt());
     }
 }
--- src/main/java/top/rafii2198/wynntils/core/type/FlatBarTexture.java
+++ src/main/java/top/rafii2198/wynntils/core/type/FlatBarTexture.java
@@ -1,6 +1,6 @@
 package top.rafii2198.wynntils.core.type;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public enum FlatBarTexture {
     CLEAR(0),
@@ -10,38 +10,37 @@ public enum FlatBarTexture {
     private final int level;
     private final int x = 119;
     private final int y = 30;
-    private final ResourceLocation resource =
-            ResourceLocation.fromNamespaceAndPath("we-helper", "textures/gui/flat_bars.png");
+    private final Identifier identifier = Identifier.fromNamespaceAndPath("we-helper", "textures/gui/flat_bars.png");
 
     FlatBarTexture(int level) {
         this.level = level;
     }
 
-    public ResourceLocation getResource() {
-        return resource;
+    public Identifier getIdentifier() {
+        return identifier;
     }
 
-    public float X1() {
-        return 0;
+    public int backgroundY1() {
+        return y * level;
     }
 
-    public float X2() {
-        return 1;
+    public int backgroundY2() {
+        return y * level + y / 2;
     }
 
-    public float backgroundY1() {
-        return (float) (y * this.level) / (values().length * y);
+    public int foregroundY1() {
+        return y * this.level + y / 2;
     }
 
-    public float backgroundY2() {
-        return (y * this.level + (float) y / 2) / (values().length * y);
+    public int foregroundY2() {
+        return y * this.level + y;
     }
 
-    public float foregroundY1() {
-        return (y * (this.level + 1) - (float) y / 2) / (values().length * y);
+    public int textureWidth() {
+        return x;
     }
 
-    public float foregroundY2() {
-        return (float) (y * (this.level + 1)) / (values().length * y);
+    public int textureHeight() {
+        return y * values().length;
     }
 }
